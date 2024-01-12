cf25d7a886398b80c77e1cc31f6f12fb2738c31e
zz <zz>
2024-01-12 18:10:07 +0800

    test

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-class Example:
+class Download:
     """
     A example node
 
@@ -73,7 +73,7 @@ class Example:
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {"Example": Example}
+NODE_CLASS_MAPPINGS = {"Download": Download}
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {"Example": "Example Node"}
+NODE_DISPLAY_NAME_MAPPINGS = {"Download": "Download Node"}
